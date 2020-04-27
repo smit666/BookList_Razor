bd3f4b0f13721a8952a4194307a90bd4de09a127
Smit Thakur <smitthakur666@gmail.com>
2020-04-27 12:36:19 +0530

    changes

---
+++ BookListRazor/Migrations/20200426095857_SeedBook.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookListRazor.Migrations
+{
+    public partial class SeedBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into book(Name,Author) values ('Test','Test Author')");
+            migrationBuilder.Sql("insert into book(Name,Author) values ('Test1','Test Author1')");
+
+            migrationBuilder.Sql("insert into book(Name,Author) values ('Test2','Test Author2')");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
